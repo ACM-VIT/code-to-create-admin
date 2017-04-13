d30303fa4d46eaaaf61b4540d616c4fcc855b29d
Sourish Banerjee <sourish.banerjeee@gmail.com>
2017-04-13 05:32:57 +0530

    Implement scan logic.

--- app/src/main/java/acm/event/code2createadmin/MainActivity.java
+++ app/src/main/java/acm/event/code2createadmin/MainActivity.java
@@ -1,13 +1,66 @@
 package acm.event.code2createadmin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import me.dm7.barcodescanner.core.IViewFinder;
+import me.dm7.barcodescanner.core.ViewFinderView;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    @BindView(R.id.options_radio_group)
+    RadioGroup optionsRadioGroup;
+
+    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.scan_button)
+    public void scan(View v) {
+        startScan();
+    }
+
+    public void startScan(){
+        scannerView = new ZXingScannerView(this) {
+            @Override
+            protected IViewFinder createViewFinderView(Context context) {
+                ViewFinderView finderView = new ViewFinderView(context);
+                finderView.setBorderColor(Color.TRANSPARENT);
+                return finderView ;
+            }
+        };
+        setContentView(scannerView);
+
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        String encodedData[] = rawResult.getText().split(" ");
+        String userId = encodedData[0];
+        String type = encodedData[1].replaceAll("\n", "").toLowerCase();
+        Log.e("message", userId + " " + type);
+
         setContentView(R.layout.activity_main);
     }
 }
