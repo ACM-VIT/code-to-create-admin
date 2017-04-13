a4fad63d0b566ae23e49558549bf8f8cdc01e5be
Sourish Banerjee <sourish.banerjeee@gmail.com>
2017-04-13 09:55:32 +0530

    Integrate reuests.

--- app/src/main/java/acm/event/code2createadmin/Fragments/ScannerFragment.java
+++ app/src/main/java/acm/event/code2createadmin/Fragments/ScannerFragment.java
@@ -1,30 +1,42 @@
 package acm.event.code2createadmin.Fragments;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.JsonObject;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import acm.event.code2createadmin.Main.MainActivity;
+import acm.event.code2createadmin.RetroAPI.RetroAPI;
 import me.dm7.barcodescanner.core.IViewFinder;
 import me.dm7.barcodescanner.core.ViewFinderView;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class ScannerFragment extends Fragment implements ZXingScannerView.ResultHandler{
     private ZXingScannerView scannerView;
     private String adminId = "VinitSourish#12";
     public boolean revert = false;
+    RetroAPI retroAPI;
+    ProgressDialog progressDialog;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -39,6 +51,7 @@ public class ScannerFragment extends Fragment implements ZXingScannerView.Result
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        retroAPI = new RetroAPI();
         scannerView = new ZXingScannerView(getActivity());
         scannerView = new ZXingScannerView(this.getActivity()) {
             @Override
@@ -79,6 +92,113 @@ public class ScannerFragment extends Fragment implements ZXingScannerView.Result
         String type = encodedData[1].replaceAll("\n", "").toLowerCase();
         Log.e("message", revert + " " + userId + " " + type);
 
-        ((MainActivity)getActivity()).loadHome();
+        if(revert)
+            revertCoupon(userId, type);
+        else
+            redeemCoupon(userId, type);
+    }
+
+    public void revertCoupon(final String userId, final String type) {
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Reverting Coupon...");
+        progressDialog.show();
+        retroAPI.observableAPIService.revertCoupon(type, userId, adminId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<JsonObject>() {
+                    @Override
+                    public void onCompleted() {}
+
+                    @Override
+                    public void onError(Throwable e) {
+                        StringWriter sw = new StringWriter();
+                        PrintWriter pw = new PrintWriter(sw);
+                        e.printStackTrace(pw);
+                        Log.e("message", sw.toString());
+                        progressDialog.dismiss();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage("Could not connect to server!")
+                                .setCancelable(false)
+                                .setPositiveButton("RETRY", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        revertCoupon(userId, type);
+                                    }
+                                })
+                                .setNegativeButton("HOME", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        ((MainActivity) getActivity()).loadHome();
+                                    }
+                                });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        String message = jsonObject.get("message").getAsString();
+                        progressDialog.dismiss();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage(message)
+                                .setCancelable(false)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        ((MainActivity) getActivity()).loadHome();
+                                    }
+                                });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                });
+    }
+
+    public void redeemCoupon(final String userId, final String type) {
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Redeeming Coupon...");
+        progressDialog.show();
+        retroAPI.observableAPIService.redeemCoupon(type, userId, adminId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<JsonObject>() {
+                    @Override
+                    public void onCompleted() {}
+
+                    @Override
+                    public void onError(Throwable e) {
+                        progressDialog.dismiss();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage("Could not connect to server!")
+                                .setCancelable(false)
+                                .setPositiveButton("RETRY", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        redeemCoupon(userId, type);
+                                    }
+                                })
+                                .setNegativeButton("HOME", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        ((MainActivity) getActivity()).loadHome();
+                                    }
+                                });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        String message = jsonObject.get("message").getAsString();
+                        progressDialog.dismiss();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage(message)
+                                .setCancelable(false)
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        ((MainActivity) getActivity()).loadHome();
+                                    }
+                                });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                });
     }
 }
